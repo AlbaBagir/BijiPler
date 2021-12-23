aef3a5619b5f1fe5a357e979d0f496b2ef45de29
AlbaBagir <87572035+AlbaBagir@users.noreply.github.com>
2021-12-23 18:16:39 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import discord
 import os
 import random
+import asyncio
+import time
+from discord import FFmpegPCMAudio
 from online import online
 from discord.ext import commands
+
 
 
 
@@ -30,7 +34,7 @@ async def tulung(ctx):
 
   em.add_field(name= "celukan", value = "biji ;Biji ;Pler ;pler ")
   em.add_field(name= "Jasa mengmisuh", value = "pisuhi,cangkemi")
-  em.add_field(name= "Fun Stuff", value = "raine, mood")
+  em.add_field(name= "Fun Stuff", value = "raine, mood, audio")
   
   await ctx.send(embed = em)
 
@@ -56,11 +60,11 @@ async def pisuhi(ctx):
 
 
 @tulung.command()
-async def cangkemi(ctx):
+async def katain(ctx):
 
-  em = discord.Embed(title = "Cangkemi", description = "men koe ra berdosa, koe iso ngengkon aku nyangkemi wong nggo koe.", color = ctx.author.color)
+  em = discord.Embed(title = "Katain", description = "men koe ra berdosa, koe iso ngengkon aku nyangkemi wong nggo koe.", color = ctx.author.color)
   
-  em.add_field(name = "Carane", value = "celuk cangkemi <njuk ngetag member>")
+  em.add_field(name = "Carane", value = "celuk katain <njuk ngetag member>")
   
   await ctx.send(embed = em)
 
@@ -79,6 +83,16 @@ async def mood(ctx):
     em = discord.Embed(title = "Mood", description = "random video", color = ctx.author.color)
 
     em.add_field(name = "Carane", value = "tulis wae +mood")
+
+    await ctx.send(embed = em)
+
+@tulung.command()
+async def audio(ctx):
+
+    em = discord.Embed(title = "audio", description = "audio gokil", color = ctx.author.color)
+
+    em.add_field(name = "Carane", value = "tulis wae bii + bantuin/cangkemi/kaget/moan/terang")
+    em.add_field(name = "contone", value = "biji kaget")
 
     await ctx.send(embed = em)
 
@@ -116,7 +130,7 @@ async def pisuhi(ctx):
 
 
 @client.command()
-async def cangkemi(ctx):
+async def katain(ctx):
    
     if (len(ctx.message.mentions) == 0):
         await ctx.send('nyangkemi sopo cok?')
@@ -134,6 +148,196 @@ async def cangkemi(ctx):
 @client.command()
 async def mood(ctx):
     await ctx.send(random.choice(emotions))
+
+@client.command()
+async def moan(ctx):
+    if (ctx.author.voice):
+      channel = ctx.message.author.voice.channel
+      vc = await channel.connect()
+      source = FFmpegPCMAudio('moan.wav')
+      player = vc.play(source)
+
+      while vc.is_playing(): #Checks if voice is playing
+          await asyncio.sleep(1) #While it's playing it sleeps for 1 second
+      else:
+          await asyncio.sleep(1) #If it's not playing it waits 1 seconds
+          while vc.is_playing(): #and checks once again if the bot is not playing
+            break #if it's playing it breaks
+          else:
+           await vc.disconnect() #if not it disconnects
+@client.command()
+async def cangkemi(ctx):
+    if (ctx.author.voice):
+      channel = ctx.message.author.voice.channel
+      vc = await channel.connect()
+      source = FFmpegPCMAudio('cangkemi.wav')
+      player = vc.play(source)
+
+      while vc.is_playing(): #Checks if voice is playing
+          await asyncio.sleep(1) #While it's playing it sleeps for 1 second
+      else:
+          await asyncio.sleep(1) #If it's not playing it waits 1 seconds
+          while vc.is_playing(): #and checks once again if the bot is not playing
+            break #if it's playing it breaks
+          else:
+           await vc.disconnect() #if not it disconnects
+      
+
+    else:
+      await ctx.send("koe nandi su")
+
+
+@client.command()
+async def terang(ctx):
+    if (ctx.author.voice):
+      channel = ctx.message.author.voice.channel
+      vc = await channel.connect()
+      source = FFmpegPCMAudio('terang.wav')
+      player = vc.play(source)
+
+      while vc.is_playing(): #Checks if voice is playing
+          await asyncio.sleep(1) #While it's playing it sleeps for 1 second
+      else:
+          await asyncio.sleep(1) #If it's not playing it waits 1 seconds
+          while vc.is_playing(): #and checks once again if the bot is not playing
+            break #if it's playing it breaks
+          else:
+           await vc.disconnect() #if not it disconnects
+      
+
+    else:
+      await ctx.send("koe nandi su")
+
+@client.command()
+async def bantuin(ctx):
+    if (ctx.author.voice):
+      channel = ctx.message.author.voice.channel
+      vc = await channel.connect()
+      source = FFmpegPCMAudio('bantuin.wav')
+      player = vc.play(source)
+
+      while vc.is_playing(): #Checks if voice is playing
+          await asyncio.sleep(1) #While it's playing it sleeps for 1 second
+      else:
+          await asyncio.sleep(1) #If it's not playing it waits 1 seconds
+          while vc.is_playing(): #and checks once again if the bot is not playing
+            break #if it's playing it breaks
+          else:
+           await vc.disconnect() #if not it disconnects
+      
+
+    else:
+      await ctx.send("koe nandi su")
+
+@client.command()
+async def kaget(ctx):
+    if (ctx.author.voice):
+      channel = ctx.message.author.voice.channel
+      vc = await channel.connect()
+      source = FFmpegPCMAudio('kaget.wav')
+      player = vc.play(source)
+
+      while vc.is_playing(): #Checks if voice is playing
+          await asyncio.sleep(1) #While it's playing it sleeps for 1 second
+      else:
+          await asyncio.sleep(1) #If it's not playing it waits 1 seconds
+          while vc.is_playing(): #and checks once again if the bot is not playing
+            break #if it's playing it breaks
+          else:
+           await vc.disconnect() #if not it disconnects
+      
+
+    else:
+      await ctx.send("koe nandi su")
+
+@client.command()
+async def ndodog(ctx):
+  if(ctx.author.voice):
+    channel = ctx.message.author.voice.channel
+    vc = await channel.connect()
+    source = FFmpegPCMAudio('ndodog.wav')
+    player = vc.play(source)
+
+    while vc.is_playing():
+        await asyncio.sleep(1)
+    else:
+        await asyncio.sleep(1)
+        while vc.is_playing():
+          break
+        else:
+          await vc.disconnect()
+
+  else:
+    await ctx.send("koe neng di su bajingani!")
+
+@client.command()
+async def clap(ctx):
+  if(ctx.author.voice):
+    channel = ctx.message.author.voice.channel
+    vc = await channel.connect()
+    source = FFmpegPCMAudio('clap.wav')
+    player = vc.play(source)
+
+    while vc.is_playing():
+        await asyncio.sleep(1)
+    else:
+        await asyncio.sleep(1)
+        while vc.is_playing():
+          break
+        else:
+          await vc.disconnect()
+
+  else:
+    await ctx.send("koe neng di su bajingani!")
+
+@client.command()
+async def kenthu(ctx):
+  if(ctx.author.voice):
+    channel = ctx.message.author.voice.channel
+    vc = await channel.connect()
+    source = FFmpegPCMAudio('kenthu.wav')
+    player = vc.play(source)
+
+    while vc.is_playing():
+        await asyncio.sleep(1)
+    else:
+        await asyncio.sleep(1)
+        while vc.is_playing():
+          break
+        else:
+          await vc.disconnect()
+
+  else:
+    await ctx.send("koe neng di su bajingani!")
+
+@client.command()
+async def messi(ctx):
+  if(ctx.author.voice):
+    channel = ctx.message.author.voice.channel
+    vc = await channel.connect()
+    source = FFmpegPCMAudio('messi.wav')
+    player = vc.play(source)
+
+    while vc.is_playing():
+        await asyncio.sleep(1)
+    else:
+        await asyncio.sleep(1)
+        while vc.is_playing():
+          break
+        else:
+          await vc.disconnect()
+
+  else:
+    await ctx.send("koe neng di su bajingani!")
+
+
+@client.command()
+async def minggat(ctx):
+    if(ctx.voice_client):
+      await ctx.guild.voice_client.disconnect()
+    else:
+      await ctx.send("????")
+
 
 
 
